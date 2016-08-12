29e48e4f01b1c699e750808c600e7bb078d11ede
NashStrife <jac.sebastien@gmail.com>
2016-08-12 14:14:46 +0200

    base services

--- public/js/services.js
+++ public/js/services.js
@@ -6,13 +6,23 @@ services.factory('Crm',['$resource', function($resource) {
 
 	let list = {
 
-		category : '',
+		category : '',	// ???
 
 		getList : function(callback) {
+			resource.query(callback);
 		},
-		addRestaurant : function(restaurant, types, pictures, callback) {
+		addCustomer : function(newCustomer, types, pictures, callback) {
+			let customer = new resource();
+            customer.name = newCustomer.name;
+            
+            customer.createdAt = Date.now();
+            // console.log(customer);
+            customer.$save(callback);
 		},
-		deleteRestaurant : function(id, callback) {
+		deleteCustomer : function(id, callback) {
+			let resource = $resource("/api/crm/"+id);
+            // callback is not required but it's better to get the different messages [error, validation,...] 
+            resource.remove(callback);
 		}
 	}
 
