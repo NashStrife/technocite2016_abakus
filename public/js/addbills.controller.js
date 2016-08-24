99aee15673a261a59c73b1ffa5ed859a1a7ee659
Sebastien  Druart <sebastiendruart@MacBook-Pro-de-Sebastien.local>
2016-08-24 10:38:10 +0200

    function facturation

--- public/js/addbills.controller.js
+++ public/js/addbills.controller.js
@@ -4,11 +4,12 @@ var abakusControllers = angular.module('addBillsControllers', []);
 
 // add the "Rest" service inside the diferent Ctrl
 abakusControllers.controller('AddBillCtrl', ['$scope', 'Client', 'Admin', function($scope, Client, Admin) {
-	
+
 	// function to clean all informations when needed
-	function voidArrays(){
+	function voidArrays() {
 		$scope.newBill = {};
-		$scope.newBill.quantite = {};
+		$scope.newBill.article = {};
+		$scope.newBill.article.quantity = 0;
 
 		$scope.articles = [];
 	}
@@ -59,18 +60,18 @@ abakusControllers.controller('AddBillCtrl', ['$scope', 'Client', 'Admin', functi
 	// define the base order when list appear
 	$scope.order = "createdAt";
 	$scope.direction = "reverse";
-	
+
 	voidArrays();
 
 	// function to calculate the amount in function of unit price and quantity
-	$scope.calcAmount = function(){
+	$scope.calcAmount = function() {
 		// console.log("it changes !");
 		var quantity = $scope.newBill.article.quantity;
 		var unitPrice = $scope.newBill.article.unitPrice;
 
 		$scope.newBill.article.amount = quantity * unitPrice;
 
-		
+
 		// console.log(unitPrice);
 		// console.log(quantity);
 		// console.log($scope.newBill.article.amount);
@@ -81,7 +82,7 @@ abakusControllers.controller('AddBillCtrl', ['$scope', 'Client', 'Admin', functi
 	// 	console.log(angular.element(event.target));
 	// 	// var target = angular.element(event.target);
 	// 	 var target = document.getElementById("facture");
-		 
+
 	// 	var cln = target.cloneNode(true);
 	// 	var wrapper = document.getElementById("facture_list");
 	// 	wrapper.appendChild(cln);
@@ -91,44 +92,51 @@ abakusControllers.controller('AddBillCtrl', ['$scope', 'Client', 'Admin', functi
 	// 		$scope.addArticle = "";
 	// 	};
 	// }
-	
+
 	// add articles to the temp list inside the form
-	$scope.addElement = function(elem){
+	$scope.addElement = function(elem) {
 		// for the list of articles
-		if(elem === 'article') {
-			$scope.articles.push({
-				'name' : $scope.newBill.article.name,
-				'description' : $scope.newBill.article.description,
-				'quantity' : $scope.newBill.article.quantity,
-				'unitPrice' : $scope.newBill.article.unitPrice,
-				'amount' : $scope.newBill.article.amount
-			});
-			console.log($scope.articles);
-			// clean the inputs when we add a new pic on the temporary array
-			// $scope.newBill.article.name
-			$scope.newBill.article.quantity = "";
-			$scope.newBill.article.amount = "";
+		if (elem === 'article') {
+			if ($scope.newBill.article.quantity) {
+				$scope.articles.push({
+					'name': $scope.newBill.article.name,
+					'description': $scope.newBill.article.description,
+					'quantity': $scope.newBill.article.quantity,
+					'unitPrice': $scope.newBill.article.unitPrice,
+					'amount': $scope.newBill.article.amount
+				});
+				console.log($scope.articles);
+				// clean the inputs when we add a new pic on the temporary array
+				// $scope.newBill.article.name
+
+				$scope.newBill.article.quantity = "";
+				$scope.newBill.article.amount = "";
+			}
+			else{
+				alert("Quantité indéfinie");
+			}
+
 		}
 	}
 
-	$scope.removeElement = function(elem, index){
-		if(elem === 'article'){
+	$scope.removeElement = function(elem, index) {
+		if (elem === 'article') {
 			$scope.articles.splice(index, 1);
 		}
 	}
-	$scope.addBill = function(isValid){
-			if(isValid){
-				Rest.addResto($scope.newResto, $scope.types, $scope.pics, function(result){
-					alert(result.message);
-					console.log(result);
-					// clean the temp Arrays after sending the form for the next one
-					voidArrays();
-				});
-				$scope.error = false;
-			} else {
-				console.log("Invalid Submit !");
-				alert("Please complete all required champs");
-				$scope.error = true;
-			}
+	$scope.addBill = function(isValid) {
+		if (isValid) {
+			Rest.addResto($scope.newResto, $scope.types, $scope.pics, function(result) {
+				alert(result.message);
+				console.log(result);
+				// clean the temp Arrays after sending the form for the next one
+				voidArrays();
+			});
+			$scope.error = false;
+		} else {
+			console.log("Invalid Submit !");
+			alert("Please complete all required champs");
+			$scope.error = true;
 		}
+	}
 }]);
